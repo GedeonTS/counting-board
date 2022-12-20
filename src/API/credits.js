01fce7de7954abca14ada45ca6f2d9f02d421a7e
GedeonTS <gedeontshobohwa1@gmail.com>
2022-12-21 01:39:05 +0200

    Add mock data for credits

---
+++ src/API/credits.js
@@ -0,0 +1,38 @@
+export const credits = [
+    {
+        "id": 1,
+        "date": "2021-01-01",
+        "product": "Beer",
+        "quantity": 1,
+        "store": "A",
+        "Client": "John Doe",
+        "status": "pending"
+    },
+    {
+        "id": 2,
+        "date": "2021-01-01",
+        "product": "Wine",
+        "quantity": 1,
+        "store": "B",
+        "Client": "Alice M",
+        "status": "pending"
+    },
+    {
+        "id": 3,
+        "date": "2021-01-01",
+        "product": "Vodka",
+        "quantity": 1,
+        "store": "C",
+        "Client": "John Doe",
+        "status": "pending"
+    },
+    {
+        "id": 4,
+        "date": "2021-01-01",
+        "product": "Whiskey",
+        "quantity": 1,
+        "store": "A",
+        "Client": "Alice M",
+        "status": "payed"
+    }
+]
