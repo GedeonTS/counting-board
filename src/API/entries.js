3fa5d5045e4cf255c7fa2ef459e1e6193edd95f8
GedeonTS <gedeontshobohwa1@gmail.com>
2022-12-20 23:58:30 +0200

    Create entries mock data file

---
+++ src/API/entries.js
@@ -0,0 +1,11 @@
+export const entries = [
+    {
+        "id": 1,
+        "name": "Sandy",
+        "store": "Store B",
+        "avatar": "https://images.unsplash.com/photo-1564460576398-ef55d99548b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+        "message": "Hi, I have a bunch of empty bottles, still waiting for the first delivery to run at full speed. I didn't have time to set up the store yet, but I'm sure it will be great. I'm looking forward to the first delivery.",
+        "date": "2021-05-01T00:00:00.000Z",
+        "type": "credit"
+    },
+]
