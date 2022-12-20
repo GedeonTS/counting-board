c7f329ad83eba9689e69db4fefd3a1865b06dde8
GedeonTS <gedeontshobohwa1@gmail.com>
2022-12-20 03:23:16 +0200

    Create popup menu

---
+++ src/components/PopupMenu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './popupMenu.css'
+
+function PopupMenu() {
+  return (
+      <div className='popup-container'>
+          PopupMenu
+      </div>
+  )
+}
+
+export default PopupMenu
