3538aa32c7784e8fb1ca7cb69a3f24bd4816afe8
GedeonTS <gedeontshobohwa1@gmail.com>
2022-12-21 01:39:36 +0200

    Add mock data for sales

---
+++ src/API/sales.js
@@ -0,0 +1,82 @@
+export const sales = [
+    {
+        "id": 1,
+        "date": "2021-01-01",
+        "products": [
+            {
+                "name": "Beer",
+                "quantity": 1
+            },
+            {
+                "name": "Wine",
+                "quantity": 1
+            },
+            {
+                "name": "Vodka",
+                "quantity": 1
+            }],
+        "store": "A",
+        "Client": "John Doe"
+    },
+    {
+        "id": 2,
+        "date": "2021-01-01",
+        "products": [
+            {
+                "name": "Beer",
+                "quantity": 1
+            },
+            {
+                "name": "Wine",
+                "quantity": 1
+            },
+            {
+                "name": "Vodka",
+                "quantity": 1
+            }],
+        "store": "B",
+        "Client": "John Doe"
+    },
+    {
+        "id": 3,
+        "date": "2021-01-01",
+        "products": [
+            {
+                "name": "Beer",
+                "quantity": 1
+            },
+            {
+                "name": "Wine",
+                "quantity": 1
+            },
+            {
+                "name": "Vodka",
+                "quantity": 1
+            }],
+        "store": "C",
+        "Client": "John Doe"
+    },
+    {
+        "id": 4,
+        "date": "2021-01-01",
+        "products": [
+            {
+                "name": "Beer",
+                "quantity": 1
+            },
+            {
+                "name": "Wine",
+                "quantity": 1
+            },
+            {
+                "name": "Vodka",
+                "quantity": 1
+            }],
+        "store": "A",
+        "Client": "Alice M"
+    }
+]
+
+
+
+        
