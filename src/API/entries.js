692ef7736caef3b388aaeabf60445352a31e5a8a
GedeonTS <gedeontshobohwa1@gmail.com>
2022-12-21 01:39:15 +0200

    Add mock data for entries

--- src/API/entries.js
+++ src/API/entries.js
@@ -1,11 +1,30 @@
 export const entries = [
     {
         "id": 1,
-        "name": "Sandy",
-        "store": "Store B",
-        "avatar": "https://images.unsplash.com/photo-1564460576398-ef55d99548b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
-        "message": "Hi, I have a bunch of empty bottles, still waiting for the first delivery to run at full speed. I didn't have time to set up the store yet, but I'm sure it will be great. I'm looking forward to the first delivery.",
-        "date": "2021-05-01T00:00:00.000Z",
-        "type": "credit"
+        "date": "2021-01-01",
+        "product": "Beer",
+        "quantity": 1,
+        "store": "A"
     },
+    {
+        "id": 2,
+        "date": "2021-01-01",
+        "product": "Wine",
+        "quantity": 1,
+        "store": "B"
+    },
+    {
+        "id": 3,
+        "date": "2021-01-01",
+        "product": "Vodka",
+        "quantity": 1,
+        "store": "C"
+    },
+    {
+        "id": 4,
+        "date": "2021-01-01",
+        "product": "Whiskey",
+        "quantity": 1,
+        "store": "A"
+    }
 ]
