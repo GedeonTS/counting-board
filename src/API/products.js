66c05b2b02a02aaa793691466d8e9859cd1f99b1
GedeonTS <gedeontshobohwa1@gmail.com>
2022-12-21 00:03:15 +0200

    Add data to the mock product file

--- src/API/products.js
+++ src/API/products.js
@@ -1,1 +1,47 @@
-export const
+export const products = [
+    {
+        "id": 1,
+        "name": "Beer",
+        "price": 1.5,
+        "image": "https://images.unsplash.com/photo-1564460576398-ef55d99548b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+        "description": "A beer is an alcoholic beverage made from fermented cereal grains or malt. It is the world's most widely consumed and oldest alcoholic drink, and the third most popular drink overall after water and tea.",
+        "category": "drink",
+        "quantity": 10
+    },
+    {
+        "id": 2,
+        "name": "Wine",
+        "price": 2.5,
+        "image": "https://images.unsplash.com/photo-1564460576398-ef55d99548b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+        "description": "Wine is an alcoholic beverage made from fermented grapes. Yeast consumes the sugar in the grapes and converts it to ethanol, carbon dioxide, and heat. Different varieties of grapes and strains of yeasts produce different styles of wine.",
+        "category": "drink",
+        "quantity": 10
+    },
+    {
+        "id": 3,
+        "name": "Vodka",
+        "price": 3.5,
+        "image": "https://images.unsplash.com/photo-1564460576398-ef55d99548b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+        "description": "Vodka is a distilled beverage composed primarily of water and ethanol, sometimes with traces of impurities and flavorings. Vodka is made by the double distillation of fermented substances such as grains, potatoes, or sometimes fruits or sugar.",
+        "category": "drink",
+        "quantity": 10
+    },
+    {
+        "id": 4,
+        "name": "Whiskey",
+        "price": 4.5,
+        "image": "https://images.unsplash.com/photo-1564460576398-ef55d99548b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+        "description": "Whiskey is a type of distilled alcoholic beverage made from fermented grain mash. Various grains are used for different varieties, including barley, malted barley, rye, malted rye, wheat, and corn.",
+        "category": "drink",
+        "quantity": 10
+    },
+    {
+        "id": 5,
+        "name": "Tequila",
+        "price": 5.5,
+        "image": "https://images.unsplash.com/photo-1564460576398-ef55d99548b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+        "description": "Tequila is a distilled beverage made from the blue agave plant, primarily in the area surrounding the city of Tequila 65 km (40 mi) northwest of Guadalajara, and in the highlands (Los Altos) of the central western Mexican state of Jalisco.",
+        "category": "drink",
+        "quantity": 10
+    }
+]
