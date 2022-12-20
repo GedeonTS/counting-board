017af79f72ac56bee76d58391ed1555724f423e7
GedeonTS <gedeontshobohwa1@gmail.com>
2022-12-21 01:38:31 +0200

    Create custom table

---
+++ src/components/Table.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './table.css'
+
+function Table() {
+  return (
+     <table className='table-container'>
+          <thead className='table-header'>
+              <tr className="table-head-row">
+                  <th className='table-head'>First Name</th>
+                  <th className='table-head'>Last Name</th>
+                  <th className='table-head'>Age</th>
+              </tr>
+          </thead>
+          <tbody className='table-body'>
+              <tr className='table-row'>
+                  <td className='table-cell'>John</td>
+                  <td className='table-cell'>Doe</td>
+                  <td className='table-cell'>25</td>
+              </tr>
+              <tr className='table-row'>
+                  <td className='table-cell'>Anna</td>
+                  <td className='table-cell'>Smith</td>
+                  <td className='table-cell'>30</td>
+              </tr>
+              <tr className='table-row'>
+                  <td className='table-cell'>Peter</td>
+                  <td className='table-cell'>Jones</td>
+                  <td className='table-cell'>35</td>
+              </tr>
+          </tbody>
+        </table>
+  )
+}
+
+export default Table
