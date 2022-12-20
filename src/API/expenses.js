437fc65dcf2330248a3de9c6f3bd6ca183993859
GedeonTS <gedeontshobohwa1@gmail.com>
2022-12-21 01:39:25 +0200

    Add mock data for expenses

---
+++ src/API/expenses.js
@@ -0,0 +1,30 @@
+export const expenses = [
+    {
+        "id": 1,
+        "date": "2021-01-01",
+        "amount": 100,
+        "description": "Food and transport",
+        "store": "A"
+    },
+    {
+        "id": 2,
+        "date": "2021-01-01",
+        "amount": 100,
+        "description": "Food and transport",
+        "store": "B"
+    },
+    {
+        "id": 3,
+        "date": "2021-01-01",
+        "amount": 100,
+        "description": "Food and transport",
+        "store": "C"
+    },
+    {
+        "id": 4,
+        "date": "2021-01-01",
+        "amount": 100,
+        "description": "Food and transport",
+        "store": "A"
+    }
+]
