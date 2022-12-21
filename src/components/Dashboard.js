5e10ba634bdc7c3e4e23d065130bac83171e1766
GedeonTS <gedeontshobohwa1@gmail.com>
2022-12-21 17:31:16 +0200

    Add content to event panel

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -15,12 +15,12 @@ function Dashboard() {
       <div className='inner-container'>
         <div className='top-panel'>
           <div className='top-left-panel'>
-           <MyChart/></div>
+            <MyChart /></div>
           <div className='top-right-panel'>
             <MyChart />
           </div>
         </div>
-        <div className='center-panel'><Table/></div>
+        <div className='center-panel'><Table /></div>
         <ul className='left-side-panel'>
           <li className='left-menu-option'>All</li>
           <li className='left-menu-option'>Credits</li>
@@ -31,7 +31,7 @@ function Dashboard() {
         <div className='right-side-upper-panel'>
           <div className='subject-Header'>
             <div className='avatar-wrapper'>
-            <img src="https://images.unsplash.com/photo-1564460576398-ef55d99548b2?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1287&amp;q=80" alt="Avatar" className="avatar-image"/>
+              <img src="https://images.unsplash.com/photo-1564460576398-ef55d99548b2?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1287&amp;q=80" alt="Avatar" className="avatar-image" />
             </div><p>Sandy - <span>Store B</span></p>
           </div>
           <p className='text-message'>Hi, I have a bunch of empty bottles,
@@ -45,7 +45,23 @@ function Dashboard() {
           </p>
           <div className='chat-action'>Reply</div>
         </div>
-        <div className='right-side-lower-panel'>right-side-lower-panel</div>
+        <div className='right-side-lower-panel'>
+
+          <div className='event-container'>
+            <div className='event-header'>
+              <p>Sale</p> <p>Store A</p>
+              
+
+            </div>
+            <div className='event-content'>
+              <p>Store A</p>
+              <p>amount: $ 100</p>
+              <p>items: 10</p>
+              <p>30 minutes ago</p>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   )
