198d508dfc42de9c3faab73be99531c50bee8e57
GedeonTS <gedeontshobohwa1@gmail.com>
2023-01-10 13:03:34 +0200

    Add scripts to the popup element

--- src/components/PopupMenu.js
+++ src/components/PopupMenu.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWindowClose } from '@fortawesome/free-solid-svg-icons'
+
 import './popupMenu.css'
 
-function PopupMenu() {
+function PopupMenu(props) {
   return (
       <div className='popup-container'>
-          PopupMenu
+      <div className='popup-inner'>
+      <FontAwesomeIcon icon={faWindowClose} className="menu-bars" onClick={()=>props.handlePopup()}/>
+      <ul className='popup-menu'>
+            <li><a>Store A</a></li>
+            <li><a>Store A</a></li>
+            <li><a>Store A</a></li>
+            <li><a>Store A</a></li>
+            <li><a>Store A</a></li>
+          </ul>
+      </div>
+          
       </div>
   )
 }
