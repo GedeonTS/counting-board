18a4a9754c44a5bb157b9ec000ab2fe589dc9e26
GedeonTS <gedeontshobohwa1@gmail.com>
2023-01-12 01:37:57 +0200

    Add scripts to the Messages section

---
+++ src/components/MessagesSession.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWindowClose } from '@fortawesome/free-solid-svg-icons'
+
+import './MessagesSession.css'
+
+function MessagesSession() {
+  return (
+      <div className='msg-session-container'>
+  <div className='msg-session-container-inner'>
+    </div> 
+      </div>
+  )
+}
+
+export default MessagesSession
